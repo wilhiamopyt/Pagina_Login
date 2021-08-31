aef2642453c914540be51ff2aba4422fc8204e85
wilhiamopyt <william.growth.opyt@gmail.com>
2021-08-31 17:28:06 -0300

    Atualizações 31-08

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
-<?php
-echo'OK';
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="index.css">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+<title>Sistema Financeiro</title>
+</head>
+<body>
+<nav id="topo">
+    <div  id="cabeçalho">
+        <img src="img/opyt.png" alt="" width="100px">
+    </div>
+</nav>
+<form action="">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <input class="form-control" type="date">
+            </div>
+            <div class="col">
+                <input class="form-control"type="text" placeholder="Descrição">      
+            </div>  
+            <div class="col">
+                <input class="form-control"type="number"placeholder="Valor">      
+            </div>
+            <div class="col">
+                <select class="form-select"name="Tipo" id="">                
+                    <option value="Boleto">Boleto</option>
+                    <option value="Dinheiro">Dinheiro</option>
+                </select>      
+            </div>
+            <div class="col">
+                <input class="btn btn-outline-secondary"type="submit" value="Cadastrar">
+        </div>              
+    </div>
+</form>
+<div>
+
+</div>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
--- login.php
+++ login.php
@@ -3,9 +3,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <title>Sistema Financeiro Opyt</title>
+    <title>Login Sistema Financeiro</title>
 </head>
 <body>
     <div>
@@ -14,21 +14,18 @@
                 <img src="img/opyt.png" alt="" width="100px">
             </div>
         </nav>
-        </div>
         <div class="container">
         <div class="row justify-content-md-center" id="teste" >
-            <div id="divcentral" class="col col-lg-2"> 
-               
-                  
+            <div id="divcentral" class="col col-lg-2">
                 <form action="dados.php"method="POST">
-                    <div><label for="user">Usuario</label>
-                    <input type="text" name="usuario" id=""/>
-                    </div>        
-                    <div><label for="password">Senha </label>
-                    <input type="password" name="senha"/>
+                    <div><label for="user" class="form-label">Usuario</label>
+                    <input type="text" name="usuario" class="form-control" id=""/>
+                    </div><br>       
+                    <div><label for="password" class="form-label">Senha </label>
+                    <input type="password" name="senha" class="form-control"/>
                     </div><br>
-                    <div id="botao">
-                    <input type="submit" value="Entrar">
+                    <div id="botao">                    
+                    <button  type="submit"class="btn btn-primary btn-lg ">Entrar</button>
                     </div>
                 </form> 
             </div>
